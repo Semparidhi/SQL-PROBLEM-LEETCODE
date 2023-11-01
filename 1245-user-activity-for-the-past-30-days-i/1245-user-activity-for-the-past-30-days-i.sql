618222830d3513b8c6af3324852b0f48f925bcd1
Semparidhi <130010535+Semparidhi@users.noreply.github.com>
2023-11-01 13:19:12 +0530

    Time: 924 ms (47.08%), Space: 0B (100%) - LeetHub

---
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,4 @@
+Select activity_date as day, count( distinct user_id) as active_users from Activity 
+WHERE DATEDIFF('2019-07-27',activity_date)<30 AND activity_date<='2019-07-27'
+group by activity_date;
+
